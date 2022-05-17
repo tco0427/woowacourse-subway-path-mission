c54d46d3d5586402648b0769b6c7b75ff715cb9b
Donggyu <a29661498@gmail.com>
2022-05-17 20:23:17 +0900

    refactor: 매직넘버 상수 적용

--- src/main/java/wooteco/subway/domain/Fare.java
+++ src/main/java/wooteco/subway/domain/Fare.java
@@ -4,6 +4,11 @@ public class Fare {
 
     public static final int BASIC_FARE = 1250;
     public static final int ADDITIONAL_FARE = 100;
+    private static final int SECOND_ADDITIONAL_FARE = 800;
+    private static final int BASIC_DISTANCE = 10;
+    private static final int SURCHARGE_BOUNDARY = 50;
+    private static final int FIRST_CHARGING_UNIT = 5;
+    private static final int SECOND_CHARGING_UNIT = 8;
 
     private final int distance;
 
@@ -12,14 +17,15 @@ public class Fare {
     }
 
     public int calculate() {
-        if (distance <= 10) {
+        if (distance <= BASIC_DISTANCE) {
             return BASIC_FARE;
         }
 
-        if (distance <= 50) {
-            return BASIC_FARE + (int) Math.ceil(((double)(distance - 10) / 5)) * ADDITIONAL_FARE;
+        if (distance <= SURCHARGE_BOUNDARY) {
+            return BASIC_FARE + (int) Math.ceil(((double)(distance - BASIC_DISTANCE) / FIRST_CHARGING_UNIT)) * ADDITIONAL_FARE;
         }
 
-        return BASIC_FARE + 800 + (int) Math.ceil(((double)(distance - 50) / 8)) * ADDITIONAL_FARE;
+        return BASIC_FARE + SECOND_ADDITIONAL_FARE
+                + (int) Math.ceil(((double)(distance - SURCHARGE_BOUNDARY) / SECOND_CHARGING_UNIT)) * ADDITIONAL_FARE;
     }
 }
