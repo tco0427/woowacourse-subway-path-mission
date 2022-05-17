cab10c851369dc6e58b442b90331bb49b07b7428
seong-wooo <seongwoo0513@uos.ac.kr>
2022-05-17 16:34:45 +0900

    feat: 최단 경로를 구하는 책임을 가지는 Path 구현

---
+++ src/main/java/wooteco/subway/domain/Path.java
@@ -0,0 +1,50 @@
+package wooteco.subway.domain;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
+
+public class Path {
+
+    private final WeightedMultigraph<Long, DefaultWeightedEdge> graph = new WeightedMultigraph<>(DefaultWeightedEdge.class);
+
+    public Path(List<Section> sections) {
+        final List<Long> stationIds = getStationIds(sections);
+
+        addVertexes(stationIds);
+        addEdges(sections);
+    }
+
+    private List<Long> getStationIds(List<Section> sections) {
+        Set<Long> stationIds = new HashSet<>();
+        for (Section section : sections) {
+            stationIds.add(section.getUpStationId());
+            stationIds.add(section.getDownStationId());
+        }
+
+        return new ArrayList<>(stationIds);
+    }
+
+    private void addVertexes(List<Long> stationIds) {
+        for (Long stationId : stationIds) {
+            graph.addVertex(stationId);
+        }
+    }
+
+    private void addEdges(List<Section> sections) {
+        for (Section section : sections) {
+            graph.setEdgeWeight(graph.addEdge(section.getUpStationId(), section.getDownStationId()),
+                    section.getDistance());
+        }
+    }
+
+
+    public List<Long> getShortestPath(Long sourceId, Long targetId) {
+        DijkstraShortestPath<Long, DefaultWeightedEdge> dijkstraShortestPath = new DijkstraShortestPath<>(graph);
+        return dijkstraShortestPath.getPath(sourceId, targetId).getVertexList();
+    }
+}
---
+++ src/test/java/wooteco/subway/domain/PathTest.java
@@ -0,0 +1,54 @@
+package wooteco.subway.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PathTest {
+
+    @DisplayName("한 노선에서 구간과 역 정보를 통해 최단 경로를 구할 수 있다.")
+    @Test
+    public void getShortestPath() {
+        // given
+        List<Section> sections = new ArrayList<>();
+        sections.add(new Section(1L, 1L, 4L, 5L, 3));
+        sections.add(new Section(2L, 1L, 1L, 2L, 3));
+        sections.add(new Section(3L, 1L, 3L, 4L, 4));
+        sections.add(new Section(4L, 1L, 2L, 3L, 4));
+
+        final Path path = new Path(sections);
+
+        // when
+        List<Long> shortestPath = path.getShortestPath(1L, 5L);
+
+        // then
+        assertThat(shortestPath).containsExactly(1L, 2L, 3L, 4L, 5L);
+    }
+
+    @DisplayName("여러 노선이 존재할 때 구간과 역 정보를 통해 최단 경로를 구할 수 있다.")
+    @Test
+    public void getShortestPath2() {
+        // given
+        List<Section> sections = new ArrayList<>();
+        sections.add(new Section(1L, 1L, 1L, 2L, 1));
+        sections.add(new Section(2L, 1L, 2L, 5L, 2));
+        sections.add(new Section(3L, 1L, 5L, 6L, 2));
+        sections.add(new Section(4L, 1L, 6L, 7L, 1));
+
+        sections.add(new Section(5L, 2L, 2L, 3L, 1));
+        sections.add(new Section(6L, 2L, 3L, 4L, 1));
+        sections.add(new Section(7L, 2L, 4L, 6L, 1));
+
+
+        final Path path = new Path(sections);
+
+        // when
+        List<Long> shortestPath = path.getShortestPath(1L, 7L);
+
+        // then
+        assertThat(shortestPath).containsExactly(1L, 2L, 3L, 4L, 6L, 7L);
+    }
+}
