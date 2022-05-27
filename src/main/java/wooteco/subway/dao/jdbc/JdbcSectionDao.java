59eeea4fa5f71f3e04716b1d6cbd34426e8c6e6f
Donggyu <a29661498@gmail.com>
2022-05-27 14:34:39 +0900

    refactor: SectionDao 에서 NamedParameterJdbcTemplate 으로 전환

--- src/main/java/wooteco/subway/dao/jdbc/JdbcSectionDao.java
+++ src/main/java/wooteco/subway/dao/jdbc/JdbcSectionDao.java
@@ -1,15 +1,14 @@
 package wooteco.subway.dao.jdbc;
 
-import static java.util.stream.Collectors.toList;
-
-import java.sql.PreparedStatement;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
+import java.util.Map;
+import java.util.stream.Collectors;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import wooteco.subway.dao.SectionDao;
 import wooteco.subway.domain.Section;
@@ -24,44 +23,62 @@ public class JdbcSectionDao implements SectionDao {
             resultSet.getLong("down_station_id"),
             resultSet.getInt("distance")
     );
+    private static final String SECTION_TABLE_NAME = "section";
+    private static final String GENERATE_KEY_COLUMN = "id";
 
-    private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+    private final SimpleJdbcInsert simpleJdbcInsert;
 
     public JdbcSectionDao(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+        this.jdbcTemplate = new NamedParameterJdbcTemplate(jdbcTemplate);
+        this.simpleJdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName(SECTION_TABLE_NAME)
+                .usingGeneratedKeyColumns(GENERATE_KEY_COLUMN);
     }
 
     @Override
     public Section save(Section section) {
-        String sql = "INSERT INTO `section` (line_id, up_station_id, down_station_id, distance) VALUES(?, ?, ?, ?)";
+        MapSqlParameterSource parameterSource = new MapSqlParameterSource();
+        parameterSource.addValue("line_id", section.getLineId());
+        parameterSource.addValue("up_station_id", section.getUpStationId());
+        parameterSource.addValue("down_station_id", section.getDownStationId());
+        parameterSource.addValue("distance", section.getDistance());
 
-        final KeyHolder keyHolder = new GeneratedKeyHolder();
-        jdbcTemplate.update(connection -> {
-            final PreparedStatement statement = connection.prepareStatement(sql, new String[]{"id"});
-            statement.setLong(1, section.getLineId());
-            statement.setLong(2, section.getUpStationId());
-            statement.setLong(3, section.getDownStationId());
-            statement.setInt(4, section.getDistance());
-            return statement;
-        }, keyHolder);
+        final long id = simpleJdbcInsert.executeAndReturnKey(parameterSource).longValue();
 
-        final Long id = Objects.requireNonNull(keyHolder.getKey()).longValue();
-
-        return new Section(id, section.getLineId(), section.getUpStationId(), section.getDownStationId(), section.getDistance());
+        return new Section(id, section.getLineId(),
+                section.getUpStationId(), section.getDownStationId(), section.getDistance());
     }
 
     @Override
     public void saveAll(List<Section> sections) {
-        final String sql = "INSERT INTO `section` (line_id, up_station_id, down_station_id, distance) VALUES (?, ?, ?, ?)";
-        final List<Object[]> parameters = generateParameters(sections);
+        final String sql = "INSERT INTO `section` (line_id, up_station_id, down_station_id, distance)"
+                + " VALUES (:lineId, :upStationId, :downStationId, :distance)";
+
+        SqlParameterSource[] parameters = getParameters(sections);
         jdbcTemplate.batchUpdate(sql, parameters);
     }
 
-    @Override
-    public List<Section> findByLineId(Long id) {
-        String sql = "SELECT * FROM `section` WHERE line_id = ?";
+    private SqlParameterSource[] getParameters(List<Section> sections) {
+        return sections.stream()
+                .map(this::makeParameterSource)
+                .collect(Collectors.toList()).toArray(SqlParameterSource[]::new);
+    }
 
-        return jdbcTemplate.query(sql, SECTION_ROW_MAPPER, id);
+    private SqlParameterSource makeParameterSource(Section section) {
+        final MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource();
+        mapSqlParameterSource.addValue("lineId", section.getLineId());
+        mapSqlParameterSource.addValue("upStationId", section.getUpStationId());
+        mapSqlParameterSource.addValue("downStationId", section.getDownStationId());
+        mapSqlParameterSource.addValue("distance", section.getDistance());
+        return mapSqlParameterSource;
+    }
+
+    @Override
+    public List<Section> findByLineId(Long lineId) {
+        String sql = "SELECT * FROM `section` WHERE line_id = :lineId";
+
+        return jdbcTemplate.query(sql, Map.of("lineId", lineId), SECTION_ROW_MAPPER);
     }
 
     @Override
@@ -74,37 +91,15 @@ public class JdbcSectionDao implements SectionDao {
 
     @Override
     public int deleteById(Long id) {
-        String sql = "DELETE FROM `section` WHERE id = ?";
+        String sql = "DELETE FROM `section` WHERE id = :id";
 
-        return jdbcTemplate.update(sql, id);
+        return jdbcTemplate.update(sql, Map.of("id",id));
     }
 
     @Override
     public int deleteByLineId(Long lineId) {
-        String sql = "DELETE FROM `section` WHERE line_id = ?";
+        String sql = "DELETE FROM `section` WHERE line_id = :lineId";
 
-        return jdbcTemplate.update(sql, lineId);
-    }
-
-    private List<Object[]> generateParameters(List<Section> sections) {
-        return sections.stream()
-                .map(this::generateParameter)
-                .collect(toList());
-    }
-
-    private Object[] generateParameter(Section section) {
-        return new Object[]{
-                section.getLineId(),
-                section.getUpStationId(),
-                section.getDownStationId(),
-                section.getDistance()
-        };
-    }
-
-    private List<Long> makeParameters(List<Long> stationIds) {
-        List<Long> parameters = new ArrayList<>();
-        parameters.addAll(stationIds);
-        parameters.addAll(stationIds);
-        return parameters;
+        return jdbcTemplate.update(sql, Map.of("lineId", lineId));
     }
 }
